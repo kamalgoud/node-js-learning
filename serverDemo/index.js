7a4a752d8d8ee419bc5732aa40a1c4054bb4448a
kamal <kamal.goud@vathana.com>
2025-01-25 12:13:31 +0530

    handling types of req

--- serverDemo/index.js
+++ serverDemo/index.js
@@ -2,14 +2,13 @@ const http = require("http")
 const fs = require("fs");
 const url = require("url")
 
-
-const myServer = http.createServer((req,res)=>{
+function myHandler(req,res){
     if(req.url === "/favicon.ico"){
         return res.end()
     }
     const myUrl = url.parse(req.url,true)
     console.log(myUrl.query);
-    fs.appendFile("./req.log",(new Date().toLocaleDateString()+`: request received ${req.url} \n`),()=>{
+    fs.appendFile("./req.log",(new Date().toLocaleDateString()+`: request received :: ${req.method} :: ${req.url} \n`),()=>{
         console.log(`request received for ${req.url}`);
     });
     switch(myUrl.pathname){//req.url
@@ -20,9 +19,14 @@ const myServer = http.createServer((req,res)=>{
             const name = myUrl.query.name;
             res.end(`No data available related to ${name} as of now !!!!!!!!!`)
             break;
+        case "/signup":
+            if(req.method === "GET"){res.end("Please send post request")}
+            else if(req.method === "{POST"){res.end("Successful")}
         default:
             res.end("404 Not found")
     }
-});
+}
+
+const myServer = http.createServer(myHandler);
 
 myServer.listen(8080,()=>console.log("server started"));
