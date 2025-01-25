d0c6e427a1139fd440a87a7a02b67bce8504df20
kamal <kamal.goud@vathana.com>
2025-01-25 13:01:16 +0530

    express intro

--- serverDemo/index.js
+++ serverDemo/index.js
@@ -1,32 +1,43 @@
 const http = require("http")
 const fs = require("fs");
 const url = require("url")
+const express = require('express')
 
-function myHandler(req,res){
-    if(req.url === "/favicon.ico"){
-        return res.end()
-    }
-    const myUrl = url.parse(req.url,true)
-    console.log(myUrl.query);
-    fs.appendFile("./req.log",(new Date().toLocaleDateString()+`: request received :: ${req.method} :: ${req.url} \n`),()=>{
-        console.log(`request received for ${req.url}`);
-    });
-    switch(myUrl.pathname){//req.url
-        case "/":
-            res.end("Homepage")
-            break;
-        case "/about":
-            const name = myUrl.query.name;
-            res.end(`No data available related to ${name} as of now !!!!!!!!!`)
-            break;
-        case "/signup":
-            if(req.method === "GET"){res.end("Please send post request")}
-            else if(req.method === "{POST"){res.end("Successful")}
-        default:
-            res.end("404 Not found")
-    }
-}
+const app = express()
 
-const myServer = http.createServer(myHandler);
+app.get("/",((req,res)=>{
+    res.end("Home page");
+}));
+
+app.get("/about",(req,res)=>{
+    res.end("about page"+req.query.name)
+});
+
+// function myHandler(req,res){
+//     if(req.url === "/favicon.ico"){
+//         return res.end()
+//     }
+//     const myUrl = url.parse(req.url,true)
+//     console.log(myUrl.query);
+//     fs.appendFile("./req.log",(new Date().toLocaleDateString()+`: request received :: ${req.method} :: ${req.url} \n`),()=>{
+//         console.log(`request received for ${req.url}`);
+//     });
+//     switch(myUrl.pathname){//req.url
+//         case "/":
+//             res.end("Homepage")
+//             break;
+//         case "/about":
+//             const name = myUrl.query.name;
+//             res.end(`No data available related to ${name} as of now !!!!!!!!!`)
+//             break;
+//         case "/signup":
+//             if(req.method === "GET"){res.end("Please send post request")}
+//             else if(req.method === "{POST"){res.end("Successful")}
+//         default:
+//             res.end("404 Not found")
+//     }
+// }
+
+const myServer = http.createServer(app);
 
 myServer.listen(8080,()=>console.log("server started"));
