19c2f0958083386012d20805a29202299efcaab6
kamal <kamal.goud@vathana.com>
2025-01-25 11:55:50 +0530

    url package for end points and req params

--- serverDemo/index.js
+++ serverDemo/index.js
@@ -1,12 +1,28 @@
 const http = require("http")
 const fs = require("fs");
-const { log } = require("console");
+const url = require("url")
+
 
 const myServer = http.createServer((req,res)=>{
-    console.log("request received");
+    if(req.url === "/favicon.ico"){
+        return res.end()
+    }
+    const myUrl = url.parse(req.url,true)
+    console.log(myUrl.query);
     fs.appendFile("./req.log",(new Date().toLocaleDateString()+`: request received ${req.url} \n`),()=>{
-        res.end("Hello!!!!!!")
+        console.log(`request received for ${req.url}`);
     });
+    switch(myUrl.pathname){//req.url
+        case "/":
+            res.end("Homepage")
+            break;
+        case "/about":
+            const name = myUrl.query.name;
+            res.end(`No data available related to ${name} as of now !!!!!!!!!`)
+            break;
+        default:
+            res.end("404 Not found")
+    }
 });
 
 myServer.listen(8080,()=>console.log("server started"));
